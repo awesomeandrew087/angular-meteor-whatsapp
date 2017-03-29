2546ed9346f82780bb44e1c4a9735593c829bbe4
Gary Boyer <gboyer.dev@gmail.com>
2017-03-29 00:14:13 -0400

    Add routes, states,  package, and some static data

--- client/scripts/lib/app.js
+++ client/scripts/lib/app.js
@@ -4,10 +4,14 @@ import 'angular-meteor';
 import 'angular-sanitize';
 import 'angular-ui-router';
 import 'ionic-scripts';
-import { Angular } from 'angular';
+import Angular  from 'angular';
+import Loader from 'angular-ecmascript/module-loader';
 import { Meteor } from 'meteor/meteor';
 
 // Modules
+import ChatsCtrl from '../controllers/chats.controller';
+import CalendarFilter from '../filters/calendar.filter';
+import RoutesConfig from '../routes';
 
 const App = 'Whatsapp';
 
@@ -16,6 +20,11 @@ Angular.module(App, [
   'angular-meteor',
   'ionic'
 ]);
+
+new Loader(App)
+  .load(ChatsCtrl)
+  .load(CalendarFilter)
+  .load(RoutesConfig);
 
 // Startup
 if (Meteor.isCordova) {
