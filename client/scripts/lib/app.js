5095a0220fc7c9b1db721eec9431698cfc464b2d
Gary Boyer <gboyer.dev@gmail.com>
2017-03-29 23:11:14 -0400

    Add chat view, controler logic, and static data

--- client/scripts/lib/app.js
+++ client/scripts/lib/app.js
@@ -4,10 +4,14 @@ import 'angular-meteor';
 import 'angular-sanitize';
 import 'angular-ui-router';
 import 'ionic-scripts';
-import { Angular } from 'angular';
+import Angular from 'angular';
+import Loader from 'angular-ecmascript/module-loader';
 import { Meteor } from 'meteor/meteor';
 
 // Modules
+import ChatsCtrl from '../controllers/chats.controller';
+import CalendarFilter from '../filters/calendar.filter';
+import RoutesConfig from '../routes';
 
 const App = 'Whatsapp';
 
@@ -17,10 +21,16 @@ Angular.module(App, [
   'ionic'
 ]);
 
+new Loader(App)
+  .load(ChatsCtrl)
+  .load(CalendarFilter)
+  .load(RoutesConfig);
+  
 // Startup
 if (Meteor.isCordova) {
   Angular.element(document).on('deviceready', onReady);
-} else {
+} 
+else {
   Angular.element(document).ready(onReady);
 }
 
